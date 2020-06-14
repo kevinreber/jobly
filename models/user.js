cb3b82c0303b5b5b5f3a3f508ccb29f77058c6f4
kevinreber <kevinreber1@gmail.com>
2020-06-14 15:23:23 -0700

    set up users model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+/** User model */
+
+/** connect to DB */
+const db = require("../db");
+const ExpressError = require("../helpers/expressError");
+const sqlForPartialUpdate = require("../helpers/partialUpdate");
+
+class User {
+
+}
+
+// export User model
+module.exports = User;
