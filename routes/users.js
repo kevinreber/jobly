e65a518e5ccd609c74b6e3f93f72de469f659355
kevinreber <kevinreber1@gmail.com>
2020-06-14 19:03:29 -0700

    added: users routes

--- routes/users.js
+++ routes/users.js
@@ -15,5 +15,92 @@ const {
 
 const router = new express.Router();
 
+/** GET / - Return handle and name for all users.
+ * res = {user: [user, ..., ...] }
+ */
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await User.getAll(req.query);
+        return res.json({
+            users
+        });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+/** POST / - Register a new user and return the newly register user. 
+ * res = {user: userData}
+ */
+router.post("/", async (req, res, next) => {
+    try {
+        /** Validate JSON schema */
+        const validate = jsonschema.validate(req.body, userNewSchema);
+        if (!validate.valid) {
+            throw new ExpressError(validate.errors.map(e => e.stack), 400);
+        }
+
+        const user = await User.register(req.body);
+        return res.status(201).json({
+            user
+        });
+
+    } catch (err) {
+        return next(err);
+    }
+})
+
+/** GET /[username] - Return a single user founded by it's [username].
+ * res = {user: userData}
+ */
+router.get("/:username", async (req, res, next) => {
+    try {
+        const user = await User.get(req.params.username);
+        return res.json({
+            user
+        });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+/** PATCH /[username] - Update an existing user and return the updated user.
+ * res = {user: userData}
+ */
+router.patch("/:username", async (req, res, next) => {
+    try {
+        if ("username" in req.body || "is_admin" in req.body) {
+            throw new ExpressError("You are not allowed to change 'username' or 'is_admin' properties.", 400);
+        }
+
+        /** Validate JSON schema */
+        const validate = jsonschema.validate(req.body, userUpdateSchema);
+        if (!validate.valid) {
+            throw new ExpressError(validate.errors.map(e => e.stack), 400);
+        }
+
+        const user = await User.update(req.params.username, req.body);
+        return res.json({
+            user
+        });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+/** DELETE /[username] - Remove an existing user and return a message.
+ * res = {message: "Company deleted"}
+ */
+router.delete("/:username", async (req, res, next) => {
+    try {
+        await User.remove(req.params.username);
+        return res.json({
+            message: "User removed"
+        })
+    } catch (err) {
+        return next(err);
+    }
+})
+
 // export routes
 module.exports = router;
