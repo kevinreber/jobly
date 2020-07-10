31bff215237a38b7b0233b779634214fe9cb26a1
kevinreber <kevinreber1@gmail.com>
2020-07-09 19:45:12 -0700

    fixed: 🐛 added await to 'User.authenticate' to avoid catching 'UnhandledPromiseRejectionWarning'

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,11 @@ const express = require("express");
 const router = new express.Router();
 const createToken = require("../helpers/createToken");
 
-router.post("/login", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
     try {
         /** is request passed in does not authenticate
          *  ExpressError() will be thrown from User */
-        const user = User.authenticate(req.body);
+        const user = await User.authenticate(req.body);
         const token = createToken(user);
         return res.json({
             token
